89fc3f69fb3bf61ea13785ffd0547a0cb4fdd46f
Alisanta <alisanta@gmail.com>
2020-09-16 10:06:10 +0700

    working one

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,23 @@ import Button from "react-bootstrap/Button";
 
 export default function Main() {
   const myName = "Alist";
+  const myGrade = "73";
+  let todayDate = new Date();
+  const inlineStyle = {
+    color: "#FF5500",
+    backgroundColor: "yellow",
+    fontSize: "18px"
+  };
   return (
     <div>
-      <h1>Hello {myName}</h1>
+      <h1>
+        Hello{" "}
+        <span style={{ color: "#FF0000", backgroundColor: "grey" }}>
+          {" "}
+          {myName}{" "}
+        </span>{" "}
+        <span style={inlineStyle}>{myGrade}</span> {todayDate.toLocaleString()}
+      </h1>
       <h2>Start editing to see some magic happen!</h2>
       <ul>
         <li>fafaee</li>
